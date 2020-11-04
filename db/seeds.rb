db5f6d7ba3ede58eca093cd234e6d935a74316d4
Max Sevenig <max.sevenig@hotmail.com>
2020-11-04 16:59:53 +0000

    Yelp MVP done, need to add bootsrtap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+CATEGORY = %w[chinese italian japanese french belgian].freeze
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating fake restaurants...'
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: CATEGORY.sample,
+  )
+end
+puts 'DONE!!'
